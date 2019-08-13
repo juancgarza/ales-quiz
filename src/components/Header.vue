526b4445e230d109cf7bf5f09fc19c0462bdd4ee
JC Garza <jcgarzar@gmail.com>
2019-08-13 09:20:46 -0500

    Add header component

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>
+        <b>Ale's Awesome Quiz App!</b>
+      </b-nav-item>
+      <b-nav-item disabled>
+        Count: 2/5
+      </b-nav-item>
+    </b-nav>
+  </div>
+</template>
