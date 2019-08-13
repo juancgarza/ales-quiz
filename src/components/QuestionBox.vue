8ad589f083c839f5c66980d5acc9c9b978cab162
JC Garza <jcgarzar@gmail.com>
2019-08-13 09:20:53 -0500

    Add questionbox component

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template slot="lead">
+        {{ question.fields["Question"] }}
+      </template>
+
+      <hr class="my-4"/>
+
+      <p>
+        List of answers
+      </p>
+
+      <b-button variant="primary">Submit</b-button>
+      <b-button @click="next" variant="success">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    question: Object,
+    next: Function
+  }
+}
+</script>
+
+
