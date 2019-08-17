48c4f267ec691bc02629f9e05762adb20ba74d60
JC Garza <jcgarzar@gmail.com>
2019-08-17 16:15:24 -0500

    Add vuesweetalert and vue countdown

--- src/main.js
+++ src/main.js
@@ -5,8 +5,16 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueSweetalert2 from 'vue-sweetalert2';
+import VueCountdown from '@chenfengyuan/vue-countdown';
 
-Vue.use(BootstrapVue,axios,VueAxios)
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+Vue.use(BootstrapVue,axios,VueAxios);
+Vue.use(VueSweetalert2);
+Vue.component(VueCountdown.name, VueCountdown);
+
 Vue.config.productionTip = false
 
 new Vue({
